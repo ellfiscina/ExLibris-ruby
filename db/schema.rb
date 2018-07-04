fb01a4a75594bdabf9381e9ec985414e89da08f9
Ellen Fiscina <ellfiscina@gmail.com>
2018-07-04 08:58:39 -0300

    database

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,14 @@
 
 ActiveRecord::Schema.define(version: 2018_06_29_125741) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -48,9 +51,9 @@ ActiveRecord::Schema.define(version: 2018_06_29_125741) do
     t.integer "pages"
     t.integer "edition"
     t.string "language"
-    t.integer "editor_id"
-    t.integer "author_id"
-    t.integer "user_id"
+    t.bigint "editor_id"
+    t.bigint "author_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "status"
@@ -61,8 +64,8 @@ ActiveRecord::Schema.define(version: 2018_06_29_125741) do
   end
 
   create_table "books_lists", id: false, force: :cascade do |t|
-    t.integer "list_id", null: false
-    t.integer "book_id", null: false
+    t.bigint "list_id", null: false
+    t.bigint "book_id", null: false
     t.index ["book_id"], name: "index_books_lists_on_book_id"
     t.index ["list_id"], name: "index_books_lists_on_list_id"
   end
@@ -98,4 +101,7 @@ ActiveRecord::Schema.define(version: 2018_06_29_125741) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "books", "authors"
+  add_foreign_key "books", "editors"
+  add_foreign_key "books", "users"
 end
