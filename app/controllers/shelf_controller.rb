5927b6110fd2b6a0f88dcd5b937abceda183773b
Ellen Fiscina <ellfiscina@gmail.com>
2018-07-11 07:19:18 -0300

    btn

--- app/controllers/shelf_controller.rb
+++ app/controllers/shelf_controller.rb
@@ -2,27 +2,22 @@ class ShelfController < HomeController
 
   def read
     @books = current_user.books.per_status(1, params[:page])
-    #title_book
   end
 
   def reading
     @books = current_user.books.per_status(2, params[:page])
-    title_book
   end
 
   def to_read
     @books = current_user.books.per_status(3, params[:page])
-    title_book
   end
 
   def own
     @books = current_user.books.per_shelf(1, params[:page])
-    title_book
   end
 
   def want
     @books = current_user.books.per_shelf(3, params[:page])
-    title_book
   end
 
   def title_book
