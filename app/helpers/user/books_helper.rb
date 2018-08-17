ab470e5aeeb0985fd5ad5872bf18ad1bbfeee791
Ellen Fiscina <ellfiscina@gmail.com>
2018-08-17 12:36:35 -0300

    color progress

--- app/helpers/user/books_helper.rb
+++ app/helpers/user/books_helper.rb
@@ -3,9 +3,9 @@ module User::BooksHelper
   def progress_alt(percentage)
     if percentage > 0.89
       alternative = 'success'
-    elsif percentage > 0.6 && percentage < 0.9
+    elsif percentage > 0.49 && percentage < 0.9
       alternative = 'primary'
-    elsif percentage < 0.4
+    elsif percentage < 0.39
       alternative = 'danger'
     else
       alternative = 'warning'
