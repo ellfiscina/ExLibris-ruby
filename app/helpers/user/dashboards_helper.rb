6234ebd9aba671888e81e121ee402bd8f2a51130
ellfiscina <ellfiscina@gmail.com>
2019-05-07 14:49:51 -0300

    add times read

--- app/helpers/user/dashboards_helper.rb
+++ app/helpers/user/dashboards_helper.rb
@@ -4,4 +4,21 @@ module User::DashboardsHelper
 
     books.sum(:pages)
   end
+
+  def most_read(lists)
+    all_books(lists).inject(Hash.new(0)) { |h,v| h[v] += 1; h }
+                    .sort_by{|k,v| v}
+                    .reverse
+                    .first(10)
+  end
+
+  private
+  def all_books(lists)
+    arr = []
+    lists.each do |list|
+      arr.push(list.books.pluck(:title))
+    end
+
+    arr.flatten
+  end
 end
